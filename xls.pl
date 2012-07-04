9e51e7ee21d789b5d929bd9315a1e33a4f584e0f
Individual User <usv299792458@gmail.com>
2012-07-04 10:29:52 +0800

    add xls.pl

---
+++ xls.pl
@@ -0,0 +1,28 @@
+BEGIN { $^W = 1 }
+use strict;
+
+use Encode ();
+
+use Spreadsheet::ParseExcel ();
+use Net::Ping::External qw(ping);
+
+my $fmt = Spreadsheet::ParseExcel::FmtDefault->new;
+    my $ips;
+    my $w = Spreadsheet::ParseExcel::Workbook->Parse( '/home/doc/IP.xls', $fmt )->{Worksheet}[0];
+    for ( my $i = $$w{MinRow}+1; defined $$w{MaxRow} && $i <= $$w{MaxRow}; $i++ ) {
+      if ( $$w{Cells}[$i][8] ) {
+        my $ip = join '.', map { ( $_->Value=~/(\d+)/ )[0] } @{$$w{Cells}[$i]}[0..3];
+        unless ( ping(host => $ip, count => 3, timeout => 2) ) {
+          $ips.= $ip . ' ' . Encode::encode( 'utf8', ${$$w{Cells}[$i]}[4]->Value ) . "\n";
+        }
+      }
+    }
+    if ($ips) {
+          my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime;
+          $year += 1900;
+          $mon+=1;
+          for ( qw/usv-10 usv-6 usv11 usv-15/ ) {
+            system "echo '$hour:$min:$sec $mday-$mon-$year НЕ ОТВЕЧАЕТ:\n$ips' | smbclient -N -M $_";
+          }
+    }
+
