8cd6c16a80ebaca19e4a3f7d4c4d04a6625ad96e
Individual User <usv299792458@gmail.com>
2012-07-04 11:59:41 +0800

    add sticky.pl

---
+++ sticky.pl
@@ -0,0 +1,31 @@
+BEGIN { $^W = 1 }
+use strict;
+
+use DateTime;
+use DateTime::Format::Natural;
+
+open DOC, 'catdoc -t -w /home/doc/0_ПАМЯТКИ_ОБЩИЕ.doc |';
+while(<DOC>) {
+ next unless /^&?([^&]+)&([^&]+)&(.*)..$/;
+  my ($date_str,$task,$who)=($1,$2,$3);
+ next if $who =~ /&/;
+  my $parser = DateTime::Format::Natural->new;
+  my $date = $parser->parse_datetime($date_str);
+
+  my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime;
+  my $now = DateTime->new( year   => $year+1900,
+                           month  => $mon+1,
+                           day    => $mday,
+                           hour   => $hour,
+                           minute => $min,
+                           second => $sec,
+                         );
+
+  my $d = $now->subtract_datetime( $date );
+  if ( $d->is_positive ) {
+    for ( qw/usv-10 usv-6 usv11 usv-15/ ) {
+      system "echo '$task\nАВТОР: $who' | smbclient -N -M $_";
+    }
+  }
+}
+
