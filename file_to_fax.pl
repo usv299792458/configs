a4e2d879c71c4025fa27fe32e0cc42e08d7186e2
Individual User <usv299792458@gmail.com>
2012-07-04 12:22:15 +0800

    add file_to_fax.pl

---
+++ file_to_fax.pl
@@ -0,0 +1,21 @@
+BEGIN { $^W = 1 }
+use strict;
+
+mkdir '/file_to_fax'
+  or $! eq 'File exists' ||
+    die 'ERROR: ustanovite konstantu DATA_DISK v pravil\'noe znachenie';
+
+for my $file ( glob '/file_to_fax/*.b64' ) { eval {
+
+ die unless $file;
+
+  ( my $name = $file ) =~ s/.*\/(.*)\..*/$1/;
+  ( my $phone_num = $name ) =~ s/.*fax-([^-]+).*/$1/;
+  rename $file, '/file_to_fax/done/'.$name.'.b64' or die;
+  $file= '/file_to_fax/done/'.$name.'.b64';
+  my @args = ("uudecode", $file);
+  if ( system(@args) == 0 ) { unlink $file }
+  else { die }
+  system "sendfax -n -d $phone_num " . '/file_to_fax/done/'."$name";
+} }
+
