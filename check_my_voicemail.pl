0bae40fd6ea1c9b48209de9fcf59545d57094263
Individual User <usv299792458@gmail.com>
2013-02-25 16:32:48 +0800

    add check_my_voicemail.pl

---
+++ check_my_voicemail.pl
@@ -0,0 +1,9 @@
+BEGIN { $^W = 1 }
+use strict;
+
+local @ARGV = "sudo find /var/spool/asterisk/voicemail/my-common/3233/INBOX/ -name msg*.txt |";
+{ local $_; while (<>) {
+  system "echo 'Голосовая почта' | smbclient -N -M komp";
+ last;
+} }
+
