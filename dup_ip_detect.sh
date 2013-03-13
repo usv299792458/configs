e4f37f762127b4ef2c72c81a15498da353a2c448
Individual User <usv299792458@gmail.com>
2013-03-13 13:55:57 +0800

    bugfix in dup_ip_detect.sh

--- dup_ip_detect.sh
+++ dup_ip_detect.sh
@@ -7,7 +7,7 @@ for i in {1..254}; do
     IPS="$IPS $IP"
   fi
 done
-if [ -n $IPS ]; then
+if [ -n "$IPS" ]; then
   for j in komp usv-6 usv11 usv-15; do
     echo -e "$(date +'%F %R')\n\nDUPLICATE IP(S): $IPS" | smbclient -N -M $j > /dev/null
   done
