f67ca921f45ff439e2191a166a785d73830b11bd
Individual User <usv299792458@gmail.com>
2013-03-14 15:06:36 +0800

    add comment to dup_ip_detect.sh

--- dup_ip_detect.sh
+++ dup_ip_detect.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# NOTE: this script will not work correctly if the duplicated address is the one of this same computer on which this script is being run. But in our case it's not a problem because we regularly test other network services which are on this computer (e.g. asterisk).
 IPS=""
 for i in {1..254}; do
   IP=192.168.40.$i
