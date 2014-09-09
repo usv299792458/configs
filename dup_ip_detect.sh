60d58e97f812bcc742935fbb38bc7e5d906557fd
Individual User <usv299792458@gmail.com>
2014-09-09 09:53:35 +0800

    fix previous commit

--- dup_ip_detect.sh
+++ dup_ip_detect.sh
@@ -8,8 +8,9 @@ for i in {1..254}; do
     IPS="$IPS $IP"
   fi
 done
+array=( 192.168.40.190 debian 192.168.40.152 usv-6 192.168.40.159 usv11 192.168.40.175 usv-15 );
 if [ -n "$IPS" ]; then
-  for j in 192.168.40.190 192.168.40.152 192.168.40.159 192.168.40.175; do
-    echo -e "$(date +'%F %R')\n\nDUPLICATE IP(S): $IPS" | smbclient -N -M debian -I $j > /dev/null
+  for j in {0..3}; do
+    echo -e "$(date +'%F %R')\n\nDUPLICATE IP(S): $IPS" | smbclient -N -M ${array[$j*2+1]} -I ${array[$j*2]} > /dev/null
   done
 fi
--- xls.pl
+++ xls.pl
@@ -23,8 +23,8 @@ my $fmt = Spreadsheet::ParseExcel::FmtDefault->new;
           my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime;
           $year += 1900;
           $mon+=1;
-          for ( qw/192.168.40.190 192.168.40.152 192.168.40.159 192.168.40.175/ ) {
-            system "echo '$hour:$min:$sec $mday-$mon-$year НЕ ОТВЕЧАЕТ:\n$ips' | smbclient -N -M debian -I $_";
+          for ( [ '192.168.40.190', 'debian' ], [ '192.168.40.152', 'usv-6' ], [ '192.168.40.159', 'usv11' ], [ '192.168.40.175', 'usv-15' ] ) {
+            system "echo '$hour:$min:$sec $mday-$mon-$year НЕ ОТВЕЧАЕТ:\n$ips' | smbclient -N -M ${$_}[1] -I ${$_}[0]";
           }
     }
 
